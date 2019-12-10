532bafa82f52d6b32a668490d3ad6974d5a95e32
Viet Anh <sututhanva@gmail.com>
2019-12-10 12:58:35 +0700

    lag

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -52,7 +52,7 @@ export class HomeComponent implements OnInit {
       }
       this.articleService.getArticle().subscribe((data)=>{
         this.articles = data;
-      })
+      }) 
     }
   }
 
